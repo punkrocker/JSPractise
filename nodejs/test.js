be2d54cc3f6649116a0b93f071697e7c5cee7174
yuhaokui <punkrock_2000@163.com>
2019-07-28 02:14:02 +0800

    带参数callback

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,16 +1,22 @@
 var events = require('events');
 
-function MyObj()
-{
+function MyObj() {
     events.EventEmitter.call(this);
+    this.warning = function (msg, msg2) {
+        this.emit("someEvents", msg, msg2);
+    }
 }
+
 MyObj.prototype.__proto__ = events.EventEmitter.prototype;
 
-function myCallback() {
+function myCallback(msg, msg2) {
     console.log('hi');
+    console.log(msg);
+    console.log(msg2);
 }
 
 var myObject = new MyObj();
 myObject.on("someEvents", myCallback);
 
-myObject.emit("someEvents");
+// myObject.emit("someEvents");
+myObject.warning('hello', 'hahahaha');
