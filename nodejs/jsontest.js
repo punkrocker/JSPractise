c332215827189a1894e3dbe7714eccbe45e7a079
punkrocker <punkrock_2000@163.com>
2019-08-04 21:55:11 +0800

    对象转json字符串

--- nodejs/jsontest.js
+++ nodejs/jsontest.js
@@ -2,3 +2,6 @@ var acc = '{"name": "zhangsan", "age": 18}';
 var accObj = JSON.parse(acc);
 console.log(accObj.name)
 console.log(accObj.age)
+
+var anotherAcc = JSON.stringify(accObj);
+console.log(anotherAcc);
