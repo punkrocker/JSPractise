85c0600fc3f969bf7cf590b01762fb0849e170c7
punkrocker <punkrock_2000@163.com>
2019-08-17 19:02:29 +0800

    写入buffer

--- nodejs/jsontest.js
+++ nodejs/jsontest.js
@@ -1,7 +1,4 @@
-var acc = '{"name": "zhangsan", "age": 18}';
-var accObj = JSON.parse(acc);
-console.log(accObj.name)
-console.log(accObj.age)
-
-var anotherAcc = JSON.stringify(accObj);
-console.log(anotherAcc);
+let buf256 = Buffer.alloc(256);
+buf256.fill(0);
+buf256.write("add some text");
+console.log(buf256.toString());
