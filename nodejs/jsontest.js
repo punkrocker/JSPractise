5db417a9c5a4d45f0fd25ddd23f7d7dae866691f
punkrocker <punkrock_2000@163.com>
2019-08-22 19:12:05 +0800

    修改buffer

--- nodejs/jsontest.js
+++ nodejs/jsontest.js
@@ -2,3 +2,8 @@ let buf256 = Buffer.alloc(256);
 buf256.fill(0);
 buf256.write("add some text");
 console.log(buf256.toString());
+buf256.write("more text", 9, 9);
+console.log(buf256.toString());
+buf256[18] = 43;
+console.log(buf256.toString());
+
