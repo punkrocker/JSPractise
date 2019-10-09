dd56996565023c8ba97076828707fb9e1be80430
punkrocker <punkrock_2000@163.com>
2019-10-09 18:06:10 +0800

    类型断言第二种

--- typescript/assert.ts
+++ typescript/assert.ts
@@ -2,3 +2,6 @@ let oneString: any = 'this is a string'
 let stringLength: number = (<string>oneString).length
 
 console.log(stringLength)
+
+let len: number = (oneString as string).length
+console.log(len)
