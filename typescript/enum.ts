24e5e65f1111f6e3918322e3caa88923eb7d8ea8
punkrocker <punkrock_2000@163.com>
2019-10-11 19:37:35 +0800

    字符串型枚举

--- typescript/enum.ts
+++ typescript/enum.ts
@@ -2,9 +2,23 @@ enum OrderStatus {
     Start = 1,
     Unpaid,
     Shipping,
-    Shipped,
-    Complete
+    Shipped = aNumber(),
+    Complete = 25
+}
+
+function aNumber(): number{
+    return 21
 }
 
 console.log(OrderStatus.Start)
 console.log(OrderStatus.Complete)
+
+function one(): string{
+    return "this"
+}
+
+
+enum Functions {
+    a = "bbbb",
+    b = 'aaaa'
+}
