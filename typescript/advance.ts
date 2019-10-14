e3b8f836925e5b3bee04748dabfc9b464d0a597a
punkrocker <punkrock_2000@163.com>
2019-10-14 23:56:30 +0800

    交叉类型

--- typescript/advance.ts
+++ typescript/advance.ts
@@ -10,3 +10,17 @@ let interfaceA: A = {
 
 console.log(interfaceA.a)
 console.log(interfaceA.b)
+
+interface B {
+    c: number,
+    d: string
+}
+
+type X = A & B
+let x: X = {
+    a: 1,
+    b: "a",
+    c: 3,
+    d: "d"
+}
+console.log(x.c)
